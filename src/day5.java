6f8ba0a705bc979d5f6505de6a86fe3d3aabf9ce
James <james.marston@acc.co.nz>
2022-12-06 23:47:30 +1300

    Completed part1 of Day5

--- src/day5.java
+++ src/day5.java
@@ -1,3 +1,4 @@
+import javax.annotation.processing.SupportedSourceVersion;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -5,11 +6,13 @@ import java.util.Scanner;
 
 public class day5 {
 
-//    String filePath = "C:\\Users\\james.marston\\IdeaProjects\\AdventOfCode\\inputFolder\\day5";
-        String filePath = "C:\\Users\\james.marston\\IdeaProjects\\AdventOfCode\\inputFolder\\day5-test";
+    String filePath = "C:\\Users\\James Work\\IdeaProjects\\AdventOfCode\\inputFolder\\day5";
+//    String filePath = "C:\\Users\\James Work\\IdeaProjects\\AdventOfCode\\inputFolder\\day5-test";
     File file = new File(filePath);
 
     ArrayList<CrateStack> crates = new ArrayList<>();
+
+    ArrayList<Action> actions = new ArrayList<>();
 
     public void loadData() throws FileNotFoundException {
         Scanner sc = new Scanner(file);
@@ -19,70 +22,103 @@ public class day5 {
             String s = sc.nextLine();
 
             // All crates loaded
-            if (s.equals("")) {
-                processCratesString(cratesInput);
+            if (s.equals("") && !cratesLoaded) {
+                crates = CrateStack.processCratesString(cratesInput);
                 cratesLoaded = true;
-                System.out.println(crates.toString());
-            }
-            if (!cratesLoaded){
-                cratesInput.add(s.toCharArray());
             } else {
-//                System.out.println(s);
+                if (!cratesLoaded){
+                    cratesInput.add(s.toCharArray());
+                } else {
+                    actions.add(new Action(s));
+                }
             }
-
-
-
 
         }
         sc.close();
     }
 
-    private void processCratesString(ArrayList<char[]> row) {
-        // Find how many columns total there are
-        char[] lastRow = row.get(row.size() - 1);
-        int lastRowLength = lastRow.length-1;
-        int totalColumns = Integer.parseInt(String.valueOf(lastRow[lastRowLength]));
-
-
-        System.out.println("rows: "+(row.size()-1));
-        System.out.println("columns: "+totalColumns);
-
-//        int currentrow = row.size()-2;
-//        System.out.println(row.get(currentrow));
-
-
-        for (int c = 0; c < totalColumns; c++) {
-            int textIndex = c*4+1;
-            crates.add(new CrateStack());
-            // Iterate the rows, -2 to consider row 0 and row for numbers
-            for (int r = row.size()-2; r !=-1; r--) {
-//                System.out.print(row.get(r)[textIndex]);
-                if (row.get(r)[textIndex] != ' ') {
-                    crates.get(c).addCrate(row.get(r)[textIndex]);
-                }
-            }
-        }
-
-        for (int i = 0; i < row.size(); i++) {
-            System.out.println(row.get(i));
+    public void processAction(){
+        for (Action a: actions) {
+            crates = a.processAction(crates);
         }
     }
+
+    public String getTopCrates(){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (CrateStack c: crates) {
+            stringBuilder.append(c.getTopCrate());
+        }
+
+        return stringBuilder.toString();
+    }
+
+
 
     public static void main(String[] args) throws FileNotFoundException {
         day5 d = new day5();
         d.loadData();
 
+        d.processAction();
+        System.out.println(d);
 
-//        System.out.println("badges score:"+d.processRucksacks());
-//        System.out.println("badges score:"+d.findBadges());
-
+        System.out.println("TopCrates: "+d.getTopCrates());
 
     }
 
+    @Override
+    public String toString() {
+        return "crates=" + crates;
+    }
+}
+
+class Action {
+    int countToMove;
+    int source;
+    int destination;
+
+    public Action(String input){
+        Scanner sc = new Scanner(input);
+
+        if (sc.hasNext()){
+            sc.next();
+            countToMove = sc.nextInt();
+            sc.next();
+            source = sc.nextInt()-1;
+            sc.next();
+            destination = sc.nextInt()-1;
+        }
+
+    }
+
+    public ArrayList<CrateStack> processAction(ArrayList<CrateStack> crates){
+        CrateStack sourceStack = crates.get(source);
+        CrateStack destinationStack = crates.get(destination);
+
+        for (int i = 0; i < countToMove; i++) {
+            Character c = sourceStack.removeCrate();
+            destinationStack.addCrate(c);
+        }
+
+        // Update crates array
+        crates.set(source, sourceStack);
+        crates.set(destination, destinationStack);
+
+        return crates;
+    }
+
+    @Override
+    public String toString() {
+        return "Action{" +
+                "countToMove=" + countToMove +
+                ", source=" + source +
+                ", destination=" + destination +
+                '}';
+    }
 }
 
 class CrateStack {
-    private ArrayList<Character> crates;
+    private final ArrayList<Character> crates;
 
     public CrateStack() {
         this.crates = new ArrayList<>();
@@ -96,6 +132,31 @@ class CrateStack {
         Character crateLetter = crates.get(crates.size()-1);
         crates.remove(crates.size()-1);
         return crateLetter;
+    }
+
+    public Character getTopCrate(){
+        return crates.get(crates.size()-1);
+    }
+
+    static public ArrayList<CrateStack> processCratesString(ArrayList<char[]> row) {
+        ArrayList<CrateStack> crates = new ArrayList<>();
+        // Find how many columns total there are
+        char[] lastRow = row.get(row.size() - 1);
+        int lastRowLength = lastRow.length-1;
+        int totalColumns = Integer.parseInt(String.valueOf(lastRow[lastRowLength]));
+
+        for (int c = 0; c < totalColumns; c++) {
+            int textIndex = c*4+1;
+            crates.add(new CrateStack());
+            // Iterate the rows, -2 to consider row 0 and row for numbers
+            for (int r = row.size()-2; r !=-1; r--) {
+                if (row.get(r)[textIndex] != ' ') {
+                    crates.get(c).addCrate(row.get(r)[textIndex]);
+                }
+            }
+        }
+
+        return crates;
     }
     
     @Override
